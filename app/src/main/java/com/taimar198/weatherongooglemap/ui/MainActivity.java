a889ab19fcc92e7ecc8e08ce862354bf59d07acd
traigiuxom9x@gmail.com <traigiuxom9x@gmail..com>
2020-09-10 07:42:57 +0700

    add toolbar on gg map and remove toolbar on app

--- app/src/main/java/com/taimar198/weatherongooglemap/ui/MainActivity.java
+++ app/src/main/java/com/taimar198/weatherongooglemap/ui/MainActivity.java
@@ -1,11 +1,15 @@
 package com.taimar198.weatherongooglemap.ui;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.location.Location;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Base64;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
@@ -14,8 +18,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -41,8 +49,13 @@ import java.net.URL;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
-        CurrentWeatherDataSource.OnFetchDataListener {
+        CurrentWeatherDataSource.OnFetchDataListener,
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean permissionDenied = false;
     private GoogleMap mMap;
     private CurrentWeatherRepository mCurrentWeatherRepository;
     private static String OWM_TILE_URL = "https://tile.openweathermap.org/map/%s/%d/%d/%d.png";
@@ -56,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         init();
+
     }
 
     private void init() {
@@ -65,31 +79,69 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mIconWeather = findViewById(R.id.icon_wether);
         mCurrentWeatherRepository = CurrentWeatherRepository.getInstance();
         mCurrentWeatherRepository.getCurrentWeather(this, "21.027763", "105.834160");
+
     }
 
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Permission not yet granted. Use requestPermissions().
+            // MY_PERMISSIONS_REQUEST_SEND_SMS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-    private TileProvider createTransparentTileProvider() {
-        return new TransparentTileOWM(tileType);
-    }
-
-    private TileProvider createTilePovider() {
-        TileProvider tileProvider = new UrlTileProvider(256, 256) {
-            @Override
-            public URL getTileUrl(int x, int y, int zoom) {
-                String fUrl = "https://openweathermap.org/img/wn/10d@2x.png";
-                URL url = null;
-                try {
-                    url = new URL(fUrl);
-                }
-                catch(MalformedURLException mfe) {
-                    mfe.printStackTrace();
-                }
-
-                return url;
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
             }
-        } ;
+        } else {
+            // Permission already granted. Enable the SMS button.
+            //new AsyncTaskTest().execute();
+            if(mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                setUpMap();
+            }
+        }
+    }
 
-        return tileProvider;
+    private void setUpMap() {
+
+        mMap.getUiSettings().setScrollGesturesEnabled(true);
+        mMap.getUiSettings().setTiltGesturesEnabled(true);
+        View locationButton = ((View) findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+        // position on right bottom
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);rlp.setMargins(0,0,30,30);
+        Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_LONG).show();
+    }
+
+    private void setOnClick() {
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+    }
     }
     /**
      * Manipulates the map once available.
@@ -104,6 +156,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        checkPermission();
+        setOnClick();
         View markerView = ((LayoutInflater)getSystemService(
                         Context.LAYOUT_INFLATER_SERVICE))
                 .inflate(R.layout.activity_main2, null);
@@ -120,11 +174,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                                 this,
                                 markerView))));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(hanoi));
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.getUiSettings().setScrollGesturesEnabled(true);
-        mMap.getUiSettings().setTiltGesturesEnabled(true);
-
-
     }
 
     private Bitmap createDrawableFromView(Context context, View view) {
@@ -144,15 +193,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onFetchDataSuccess(CurrentWeather data) {
-//        if (mIconWeather == null) {
-//            mIconWeather = findViewById(R.id.icon_wether);
-//            mIconWeather.setImageResource(R.drawable.custom_marker);
-//        }
+        if (mIconWeather == null) {
+            mIconWeather = findViewById(R.id.icon_wether);
+            System.out.println(mIconWeather == null);
+        }else {
+
+            mIconWeather.setImageResource(R.drawable.custom_marker);
+        }
 
     }
 
     @Override
     public void onFetchDataFailure(Exception e) {
 
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        System.out.println("location" + location.getLatitude() +"---" + location.getLongitude());
     }
 }
